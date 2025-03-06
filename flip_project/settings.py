a810137feec2e38ce46691254d056825845534d1
kadmon76 <kadmon76@gmail.com>
2025-03-06 12:56:27 +0200

    Set up Django project structure and settings

--- flip_project/settings.py
+++ flip_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'spelling_game',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Media files (User uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Templates
+TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'templates')]
